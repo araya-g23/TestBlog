b6069d406c6568e246e9a614a7aeef7aef8c277e
Eyob <eyobgeberkidan@gmail.com>
2025-03-16 03:05:51 +0000

    page is working now

--- database/migrations/create_matches_table.php
+++
@@ -1,9 +0,0 @@
-<?php
-Schema::create('matches', function (Blueprint $table) {
-    $table->id();
-    $table->unsignedBigInteger('home_team_id');
-    $table->unsignedBigInteger('away_team_id');
-    $table->date('date');
-    $table->string('result')->nullable();
-    $table->timestamps();
-});
